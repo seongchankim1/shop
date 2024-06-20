edd43cef5007879199607176e2bd6e2cef9d4684
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-20 10:55:12 +0900

    Feat: ControllerUtil 구현

---
+++ src/main/java/com/sparta/bunga6/util/ControllerUtil.java
@@ -0,0 +1,37 @@
+package com.sparta.bunga6.util;
+
+import com.sparta.bunga6.base.dto.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Slf4j
+public final class ControllerUtil {
+
+    public static ResponseEntity<CommonResponse<?>> getFieldErrorResponseEntity(BindingResult bindingResult, String msg) {
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            log.error("{} field : {}", fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest()
+                .body(CommonResponse.<List<FieldError>>builder()
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .msg(msg)
+                        .data(fieldErrors)
+                        .build());
+    }
+
+    public static ResponseEntity<CommonResponse<?>> getResponseEntity(Object response, String msg) {
+        return ResponseEntity.ok().body(CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .msg(msg)
+                .data(response)
+                .build());
+    }
+
+}
