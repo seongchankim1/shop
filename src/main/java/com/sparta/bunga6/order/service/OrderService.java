97a94db76bdd15a34b98bbbb64428a658b19b347
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-25 01:48:47 +0900

    Feat: 주문 취소 기능, 정적 팩토리 메서드 추가

--- src/main/java/com/sparta/bunga6/order/service/OrderService.java
+++ src/main/java/com/sparta/bunga6/order/service/OrderService.java
@@ -1,110 +1,83 @@
 package com.sparta.bunga6.order.service;
 
-import java.util.List;
-
+import com.sparta.bunga6.delivery.Delivery;
+import com.sparta.bunga6.order.dto.OrderAddrRequest;
+import com.sparta.bunga6.order.dto.OrderRequest;
+import com.sparta.bunga6.order.entity.Order;
+import com.sparta.bunga6.order.repository.OrderRepository;
+import com.sparta.bunga6.orderline.OrderLine;
+import com.sparta.bunga6.product.entity.Product;
+import com.sparta.bunga6.product.service.ProductService;
+import com.sparta.bunga6.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sparta.bunga6.jwt.JwtProvider;
-import com.sparta.bunga6.order.dto.AddressRequest;
-import com.sparta.bunga6.order.dto.OrderCreateRequest;
-import com.sparta.bunga6.order.dto.OrderLineRequest;
-import com.sparta.bunga6.delivery.Delivery;
-import com.sparta.bunga6.delivery.DeliveryStatus;
-import com.sparta.bunga6.order.entity.Order;
-import com.sparta.bunga6.orderline.OrderLine;
-import com.sparta.bunga6.order.entity.OrderStatus;
-import com.sparta.bunga6.user.entity.User;
-import com.sparta.bunga6.order.repository.DeliveryRepository;
-import com.sparta.bunga6.order.repository.OrderLineRepository;
-import com.sparta.bunga6.order.repository.OrderRepository;
-import com.sparta.bunga6.order.repository.ProductRepository;
-import com.sparta.bunga6.user.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class OrderService {
 
-	private final OrderRepository orderRepository;
-	private final UserRepository userRepository;
-	private final JwtProvider jwtProvider;
-	private final ProductRepository productRepository;
-	private final DeliveryRepository deliveryRepository;
-	private final OrderLineRepository orderLineRepository;
-	private Long count;
+    private final OrderRepository orderRepository;
+    private final ProductService productService;
 
+	/**
+	 * 주문 생성
+	 */
 	@Transactional
-	public Order createOrder(OrderCreateRequest orderRequest, User user) {
-		// Order 생성
-		Order order = new Order(user);
-		order.updateStatus(OrderStatus.ORDERED);
+	public Order createOrder(OrderRequest request, User user) {
+		Product product = productService.findProduct(request.getProductId());
+		OrderLine orderLine = OrderLine.of(product, product.getPrice(), request.getCount());
+		Delivery delivery = new Delivery(request.getAddress());
 
-		// Delivery 생성
-		Delivery delivery = new Delivery(user);
-		delivery.updateStatus(DeliveryStatus.PREPARING);
-		order.setDelivery(delivery);
+		Order order = Order.of(user, delivery, orderLine);
 
-		for (OrderLineRequest orderLineRequest : orderRequest.getOrderLines()) {
-			// ID로 Product 검색
-			Product product = productRepository.findById(orderLineRequest.getProductId())
-				.orElseThrow(() -> new IllegalArgumentException("일치하는 상품이 없습니다."));
-
-			// OrderLine 생성
-			OrderLine orderLine = new OrderLine(order, product, orderLineRequest.getCount());
-
-			// Order에 OrderLine 추가
-			order.addOrderLine(orderLine);
-		}
-		// Order 저장
 		orderRepository.save(order);
 
 		return order;
 	}
 
-	public Order getOrder(Long orderId, User user) {
-		Order order = orderRepository.findById(orderId)
-			.orElseThrow(() -> new IllegalArgumentException("해당 주문을 찾을 수 없습니다."));
-		if (!user.getId().equals(order.getUser().getId())) {
-			throw new IllegalArgumentException("자신의 주문만 조회할 수 있습니다.");
-		}
-		return order;
+	/**
+	 * 전체 주문 조회 (관리자 전용)
+	 */
+	public Page<Order> findAllOrders(Pageable pageable, User admin) {
+		admin.validateAdmin();
+		return orderRepository.findAll(pageable);
 	}
 
-	public List<Order> getAllOrders(User user) {
-		List<Order> ordersList = orderRepository.findByUser(user);
-		return ordersList;
-	}
-
-	@Transactional
-	public Order updateOrder(AddressRequest requestDto,Long orderId, User user) {
-		Order order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("일치하는 주문이 없습니다."));
-		if (!user.getId().equals(order.getUser().getId())) {
-			throw new IllegalArgumentException("자신의 주문만 수정 가능합니다.");
-		}
-
-		Delivery delivery = order.getDelivery();
-		delivery.updateAddress(requestDto);
-
-		deliveryRepository.save(delivery);
+    /**
+     * 주문 조회
+     */
+    public Order findOrder(Long orderId, User user) {
+		Order order = orderRepository.findById(orderId).orElseThrow(() ->
+				new NullPointerException("해당 주문이 존재하지 않습니다."));
+		order.verifyUser(user);
 
 		return order;
-	}
+    }
 
-	@Transactional
-	public Order deleteOrder(Long orderId, User user) {
-		Order order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("일치하는 주문이 없습니다."));
-		if (!user.getId().equals(order.getUser().getId())) {
-			throw new IllegalArgumentException("자신의 주문만 삭제 가능합니다.");
-		}
+	/**
+     * 배송 주소 변경
+     */
+    @Transactional
+    public Order updateAddress(OrderAddrRequest request, User user) {
+        Order order = findOrder(request.getOrderId(), user);
+        order.updateAddress(request.getAddress());
 
-		orderRepository.delete(order);
+        return order;
+    }
 
-		return order;
-	}
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public Long cancelOrder(Long orderId, User user) {
+        Order order = findOrder(orderId, user);
+        order.cancel();
+
+        return orderId;
+    }
+
 }
-
